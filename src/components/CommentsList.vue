a32de84a671eef55577679c08de995a88c8f5739
Nico :D <nicopereyra855@gmail.com>
2024-10-15 16:55:25 -0300

    Se puede ver las fechas en los comentarios de los usuarios

--- src/components/CommentsList.vue
+++ src/components/CommentsList.vue
@@ -8,6 +8,19 @@ export default {
             default: () => [],
         },
     },
+    methods: {
+    formatDate(timestamp) {
+        const date = new Date(timestamp.seconds * 1000 + Math.floor(timestamp.nanoseconds / 1000000));
+        const formatter = new Intl.DateTimeFormat('es-AR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+        return formatter.format(date).replace(',', '');
+    }
+},
 };
 </script>
 
@@ -19,6 +32,7 @@ export default {
                 <div>
                     <router-link :to="`/profile/${comment.userId}`" class="font-bold">{{ comment.username }}</router-link>
                     <p>{{ comment.text }}</p>
+                    <p class="text-gray-400">{{ formatDate(comment.createdAt) }}</p>
                 </div>
             </div>
         </li>
